7b22ad09fd5ce0333330222f1be51d1d264c97d7
nbhatt22 <43207133+nbhatt22@users.noreply.github.com>
2018-10-03 08:38:38 -0400

    Add files via upload

---
+++ src/Favorites.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Favorites {
+
+	public static void main(String[] args) 
+	{
+		Scanner jawn = new Scanner(System.in);
+		System.out.print("Please enter your name: ");
+		String name = jawn.nextLine();
+	}
+
+}
