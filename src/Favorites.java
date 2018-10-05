bdc6c7a3ead5e8eb989e9b459008bf83a5419c02
nbhatt22 <nbhatt22@HS-MATH2-30.gvsd.org>
2018-10-05 08:16:22 -0400

    scanner practice commit

--- src/Favorites.java
+++ src/Favorites.java
@@ -7,6 +7,19 @@ public class Favorites {
 		Scanner jawn = new Scanner(System.in);
 		System.out.print("Please enter your name: ");
 		String name = jawn.nextLine();
+		System.out.print("Please enter your favorite color: ");
+		String favColor = jawn.nextLine();
+		System.out.print("Please enter your favorite food: ");
+		String favFood = jawn.nextLine();
+		System.out.print("Please enter your favorite subject: ");
+		String favSubject = jawn.nextLine();
+		
+		System.out.println("\n\n\n\n" + name + ":");
+		System.out.println("\tFavorite Color: " + favColor);
+		System.out.println("\tFavorite Food: " + favFood);
+		System.out.println("\tFavorite Subject: " + favSubject);
+		
 	}
+	
 
 }
